339383b55bdc5b620b630859b5a5cbb6c6f2e3e9
Matthew Khoo <mattkhoo1234@gmail.com>
2023-06-05 21:47:04 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {styled, darkTheme} from './ui/stitches.config';
+import Bubbles from './ui/bubbles';
+import { 
+  HorizontalFlexBoxWithColor, 
+  ParagraphBox, 
+  MiniParagraphBox,
+  TopBox,
+  Content,
+  BraggingBox,
+  HorizontalFlexBox,
+  InteranalParagraphBox,
+  InteranalParagraphBoxDisapear,
+  InteranalParagraphBoxDisapearLater,
+  Top,
+  Bottom 
+} from './ui/components';
 
 function App() {
+  const Bg = styled('div', {
+    backgroundColor: '$background',
+    height: '100%',
+    textAlign: 'center',
+    '@vbp1': {
+      height: '100vh',
+    },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Bg>
+    <Content>
+      <TopBox>
+        <HorizontalFlexBoxWithColor>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <ParagraphBox style={{paddingBottom: "20px"}}>
+              <b>
+                Segmenation and
+                <br></br>
+                Classification of Climbing Holds
+              </b>
+            </ParagraphBox>
+            <MiniParagraphBox>
+              By: Matthew Khoo and Alessandro Bifulco
+            </MiniParagraphBox>
+          </div>
+          <ParagraphBox >
+            {/* <Bubbles /> */}
+            <img src='/public/holds.png' alt='logo' />
+          </ParagraphBox>
+        </HorizontalFlexBoxWithColor>
+          <div
+            style={{
+              background: 'rgb(27, 32, 48)',
+              width: '100vw',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              flex: '1 1 ',
+              height: '100%',
+            }}
+          >
+            <BraggingBox>
+              <HorizontalFlexBox>
+                <InteranalParagraphBoxDisapearLater>
+                  <Top>100+</Top>
+
+                  <Bottom> Errors </Bottom>
+                </InteranalParagraphBoxDisapearLater>
+
+                <InteranalParagraphBoxDisapear>
+                  <Top>10+h</Top>
+                  <Bottom>Training time </Bottom>
+                </InteranalParagraphBoxDisapear>
+
+                <InteranalParagraphBox>
+                  <Top>20/20</Top>
+                  <Bottom>Vision</Bottom>
+                </InteranalParagraphBox>
+              </HorizontalFlexBox>
+            </BraggingBox>
+
+          </div>
+        </TopBox>
+      </Content>
+    </Bg>
   );
 }
 
---
+++ src/ui/components.js
@@ -0,0 +1,171 @@
+import { styled } from "./stitches.config";
+
+export const Content = styled('div', {
+    height: 'inherit',
+    display:"flex", flexDirection:"column", 
+    '@bp3': {
+        alignItems:"center",
+      },
+   
+   
+})
+
+export const TopBox = styled('div', {
+    width: "100vw",
+    display: "flex",
+    flex:"1 1",
+    flexDirection: "column",
+  
+    alignItems: "center",
+    background: "linear-gradient(45deg, #4568dc, 68%, #b06ab3)"
+
+})
+
+export const HorizontalFlexBoxWithColor = styled('div', {
+    display: "flex",
+    alignItems:"center",
+    paddingTop:"20px",
+    paddingBottom:"20px",
+    marginBottom: "15px",
+    width: "1380",
+    flexDirection: "column-reverse",
+    minHeight:"350px",
+    borderRadius: "10px",
+    fontWeight: "400",
+    justifyContent:"space-between",
+    '@bp3': {
+        width:"1000px",
+      },
+
+      '@bp0': {
+        flexDirection:"column",
+      },
+})
+
+export const ParagraphBox = styled('div', {
+    display: "flex",
+    fontSize:"$header1",
+    color:"$antibackground",
+    fontFamily: " Inter, Arial",
+    width: "1380",
+    textAlign:"left",
+    paddingInline: "20px",
+    flexDirection: "column",
+    paddingBottom:"40px",
+   
+})
+
+export const MiniParagraphBox = styled('div', {
+    display: "flex",
+    fontSize:"15px",
+    fontFamily: " Inter, Arial",
+    width: "1380",
+    textAlign:"left",
+    paddingInline: "20px",
+    color: "#e0e0e0",
+    paddingBottom:"40px",
+    fontWeight:"5",
+})
+
+export const BraggingBox = styled('div', {
+    display: "flex",
+    alignItems:"center",
+    padding:"",
+    backdropFilter: "blur(15px)",
+    paddingBottom: "10px",
+    paddingTop:"10px",
+    marginTop:"-40px",
+    marginBottom:"40px",
+    borderRadius:"10px",
+    width: "1380",
+    flexDirection: "column",
+    background:"rgb(255,255,255,0.3)",
+    mixBlendMode: "luminosity",
+    filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+    width:"980px",
+    '@bp0': {
+        width:"90%",
+    }
+    
+})
+
+export const InteranalParagraphBox = styled('div', {
+
+    display: "flex",
+    justifyContent:"center",
+    alignItems:"center",
+    padding:"",
+    fontSize:"30px",
+    flexDirection: "column",
+    height: "80px",
+    color:"#FFFFFF",
+    '@bp3': {
+   
+      },
+})
+
+export const InteranalParagraphBoxDisapear = styled('div', {
+
+    display: "flex",
+    justifyContent:"center",
+    alignItems:"center",
+    padding:"",
+    fontSize:"30px",
+
+    // maxWidth: "1000px",
+    flexDirection: "column",
+    height: "80px",
+    color:"#FFFFFF",
+    
+    '@bp630': {
+        display:"none"
+      },
+   
+    
+})
+
+export const InteranalParagraphBoxDisapearLater = styled('div', {
+
+    display: "flex",
+    justifyContent:"center",
+    alignItems:"center",
+    padding:"",
+    fontSize:"30px",
+
+    // maxWidth: "1000px",
+    flexDirection: "column",
+    height: "80px",
+    color:"#FFFFFF",
+    
+    '@bp400': {
+        display:"none"
+      },
+   
+    
+})
+
+export const HorizontalFlexBox = styled('div', {
+
+    display: "flex",
+    alignItems:"center",
+    padding:"",
+   
+    width: "100%",
+    justifyContent:"space-evenly",
+    flexDirection: "row",
+
+      '&::-webkit-scrollbar': { 
+        display: "none", 
+    }
+})
+
+export const Top = styled('div', {
+    fontSize:"$header1",
+    color:"#FFFFFF",
+    
+})
+
+export const Bottom = styled('div', {
+    fontSize:"$title",
+  
+})
